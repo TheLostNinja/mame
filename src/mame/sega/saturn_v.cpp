d50a58bef048407e401dcad1497e259b70f6fc10
TheLostNinja <116140256+TheLostNinja@users.noreply.github.com>
2026-02-06 15:59:11 +0300

    Update saturn_v.cpp

--- src/mame/sega/saturn_v.cpp
+++ src/mame/sega/saturn_v.cpp
@@ -2293,10 +2293,17 @@ int saturn_state::vdp1_start()
 	// save state
 	save_pointer(NAME(m_vdp1_regs), 0x020/2);
 	save_pointer(NAME(m_vdp1_vram), 0x100000/4);
+	save_item(NAME(m_vdp1.framebuffer_mode));
+	save_item(NAME(m_vdp1.framebuffer_double_interlace));
 	save_item(NAME(m_vdp1.fbcr_accessed));
+	save_item(NAME(m_vdp1.framebuffer_width));
+	save_item(NAME(m_vdp1.framebuffer_height));
 	save_item(NAME(m_vdp1.framebuffer_current_display));
 	save_item(NAME(m_vdp1.framebuffer_current_draw));
 	save_item(NAME(m_vdp1.framebuffer_clear_on_next_frame));
+	save_item(NAME(m_vdp1.lopr));
+	save_item(NAME(m_vdp1.copr));
+	save_item(NAME(m_vdp1.ewdr));
 	save_item(NAME(m_vdp1.local_x));
 	save_item(NAME(m_vdp1.local_y));
 	machine().save().register_postload(save_prepost_delegate(FUNC(saturn_state::vdp1_state_save_postload), this));
@@ -8557,6 +8564,14 @@ int saturn_state::vdp2_start()
 	save_pointer(NAME(m_vdp2_regs), 0x040000/2);
 	save_pointer(NAME(m_vdp2_vram), 0x100000/4);
 	save_pointer(NAME(m_vdp2_cram), 0x080000/4);
+	save_item(NAME(dotsel));
+	save_item(NAME(odd));
+	save_item(NAME(h_count));
+	save_item(NAME(v_count));
+	save_item(NAME(exltfg));
+	save_item(NAME(exsyfg));
+	save_item(NAME(old_crmd));
+	save_item(NAME(old_tvmd));
 	machine().save().register_postload(save_prepost_delegate(FUNC(saturn_state::vdp2_state_save_postload), this));
 
 	return 0;
